3a479679219d9cd50b273a8a562cbd5396b8a37e
mh-saeed <saeedkhan111244@gmail.com>
2021-04-22 23:42:42 +0500

    updated

--- src/time.js
+++ src/time.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function IsMorning(props) {
     return (
-        <h1>Good {props.dayTime} {props.dayTime == `Morning` ? `🌄` : ``}</h1>
+        <h1>Good {props.dayTime} {props.dayTime === `Morning` ? `🌄` : `🌃`}</h1>
     );
 }
 export default IsMorning;
