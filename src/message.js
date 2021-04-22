f3d3c3678dda87838a6a51ad8f09cb368d74c8af
mh-saeed <saeedkhan111244@gmail.com>
2021-04-22 23:10:18 +0500

    other components created

---
+++ src/message.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Message(props){
+    return(
+        <h2>Value of counter is {props.counter}</h2>
+    );
+}
+export default Message;
---
+++ src/time.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function IsMorning(props){
+    return(
+        <h1>Good {props.dayTime}</h1>
+    );
+}
+export default IsMorning;
